95398f20ff17824f8a07533817324f6827e466d4
hyesulee608 <40760917+Hyesu68@users.noreply.github.com>
2023-09-17 17:06:50 -0700

    1.1 Add contents

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,18 +7,29 @@ const Project = () => {
       <div className="project_row">
         <div className="project_box">
           <div className="project_image">
-            <a href="https://github.com/Hyesu68/MyMovieStar">
+            <a href="https://github.com/Hyesu68/Picstagram">
               <img
                 className="project_img"
                 alt="mymoviestar"
-                src="https://user-images.githubusercontent.com/40760917/241353973-99e351d9-1b4c-4366-be2f-2309585401e6.jpg"
+                src="https://user-images.githubusercontent.com/40760917/268475559-9bfb557b-9e5b-4d81-9381-e0a47592829f.jpeg"
               />
             </a>
           </div>
           <div className="box_info">
             <h1 className="box_title">Social Media App</h1>
             <p className="box_company">Picstagram</p>
-            <p className="box_content">content</p>
+            <p className="box_content">
+              Picstagram is a React-based web application designed with a
+              primary focus on mobile devices, offering a seamless experience
+              for photo sharing and social interaction.
+            </p>
+            <a href="https://github.com/Hyesu68/Picstagram" alt="github">
+              <img
+                src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                alt="github"
+                class="icon_image"
+              />
+            </a>
           </div>
         </div>
 
@@ -39,6 +50,13 @@ const Project = () => {
               A movie review application that allows users to explore current
               showing movies, search for movies, and write reviews about them.
             </p>
+            <a href="https://github.com/Hyesu68/MyMovieStar" alt="github">
+              <img
+                src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                alt="github"
+                class="icon_image"
+              />
+            </a>
           </div>
         </div>
 
@@ -59,6 +77,13 @@ const Project = () => {
               Android social media application that enables users to connect
               with others, share posts, and engage in conversations.
             </p>
+            <a href="https://github.com/Hyesu68/BerryMe" alt="github">
+              <img
+                src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                alt="github"
+                class="icon_image"
+              />
+            </a>
           </div>
         </div>
       </div>
