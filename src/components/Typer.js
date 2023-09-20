b7f02133ac6c8da88510928e245fef7638b85b05
hyesulee608 <40760917+Hyesu68@users.noreply.github.com>
2023-09-19 18:40:48 -0700

    1.3 Add contents

--- src/components/Typer.js
+++ src/components/Typer.js
@@ -3,7 +3,7 @@ const Typer = () => {
     <div className="typer">
       <div className="typer_black">
         <h3>Software Engineer</h3>
-        <h1 className="typer_text">Hyesu Lee Portfolio</h1>
+        <h1 className="typer_text">Hyesu Lee's Portfolio</h1>
       </div>
     </div>
   );
